7d893bb6f8d8c9e35b99002c7d1da9bdfc795a91
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-07-07 17:21:48 +0200

    esercizio dadi estrazione numeri riesce

---
+++ dadi/js/main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    let person = prompt(Math.floor(Math.random() * 6) + 1);
+    if (person != null) {
+        document.getElementById("you").innerHTML = "il tuo numero è" + person;
+      }
+  }
+
+  function myFunction() {
+    let person = prompt(Math.floor(Math.random() * 6) + 1);
+    if (person != null) {
+        document.getElementById("pc").innerHTML = "il numero del Bot è" + person;
+      }
+  }
