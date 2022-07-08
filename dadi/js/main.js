37f73e9a5ad40809e2fbc338cf569c16a898db13
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-07-08 10:44:06 +0200

    correzione

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -11,3 +11,18 @@ function myFunction() {
         document.getElementById("pc").innerHTML = "il numero del Bot è" + person;
       }
   }
+
+/*correzione */
+const numeroUtente = Math.floor(Math.random() * 6) + 1;
+alert('il giocatore ha lanciato il dado ed è uscito il numero' + numeroUtente);
+
+const numeroComputer = Math.floor(Math.random() * 6) + 1;
+alert('il computer ha lanciato il dado ed è uscito il numero' + numeroComputer)
+
+if (numeroComputer > numeroUtente) {
+  alert('vince il computer');
+} else if (numeroUtente > numeroComputer) {
+  alert("Vince l'utente");
+} else {
+  alert('pareggio');
+}
